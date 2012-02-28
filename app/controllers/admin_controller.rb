3ddf54a2249fb62afa734801be6417b1d44bfac7
Jack Chu <kamuigt@gmail.com>
2012-02-28 18:21:56 -0500

    remove navigation cache (related to #15)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,25 +1,13 @@
 class AdminController < ApplicationController
   helper_method :sort_column, :sort_direction
   before_filter :load_project
-  append_before_filter :authenticate_user!, :navigation
+  append_before_filter :authenticate_user!
   layout 'admin'
 
   protected
   def load_project
     project_name = (controller_name == "projects") ? params[:id] : params[:project_id]
     @project = project_name && Project.find_by_name(project_name)
-  end
-
-  def navigation
-    if @project
-      begin
-        @project_association_count = Rails.cache.read("project_association_count")
-        Resque.enqueue(NavigationCache) unless @project_association_count
-      rescue Exception => e
-        logger.error "Can't read from Rails cache: #{e}"
-        @project_association_count = nil
-      end
-    end
   end
 
   def sort_column
