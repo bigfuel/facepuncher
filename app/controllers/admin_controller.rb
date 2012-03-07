8162cfafbd4525666bf9e2085bac8c41bb0872bc
Winter Lee <wintafay@gmail.com>
2012-03-06 21:04:08 -0500

    fixed load project, project route, and admin project path

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,13 @@
 class AdminController < ApplicationController
   helper_method :sort_column, :sort_direction
-  append_before_filter :authenticate_user!
+  append_before_filter :authenticate_user!, :load_project
   layout 'admin'
 
   protected
+  def load_project
+    @project = params[:project_id] && Project.find_by_name(params[:project_id])
+  end
+
   def sort_column
     params[:sort] || "created_at"
   end
