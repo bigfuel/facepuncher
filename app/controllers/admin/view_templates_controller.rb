ccaa1f5cfb36f771956eac83024141c471f8951b
Winter Lee <wintafay@gmail.com>
2012-03-13 18:42:05 -0400

    updated release and view template controller with respond_with

--- app/controllers/admin/view_templates_controller.rb
+++ app/controllers/admin/view_templates_controller.rb
@@ -1,65 +1,40 @@
 class Admin::ViewTemplatesController < AdminController
   def index
     @view_templates = @project.view_templates.order_by([sort_column, sort_direction]).page(params[:page])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @view_templates }
-    end
+    respond_with @view_templates
   end
 
   def new
-    @view_template = @project.view_templates.new
+    @release = @project.view_templates.new
   end
 
   def edit
-    @view_template = @project.view_templates.find(params[:id])
+    @release = @project.view_templates.find(params[:id])
   end
 
   def show
-    @view_template = @project.view_templates.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @view_template }
-    end
+    @release = @project.view_templates.find(params[:id])
+    respond_with @release
   end
 
   def create
-    @view_template = @project.view_templates.new(params[:view_template])
-
-    respond_to do |format|
-      if @view_template.save
-        format.html { redirect_to([:admin, @project, @view_template], notice: 'View Template was successfully created.') }
-        format.json { render json: @view_template, status: :created, location: @view_template }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @view_template.errors, status: :unprocessable_entity }
-      end
-    end
+    @release = @project.view_templates.new(params[:release])
+    @release.save
+    respond_with @release, location: [:admin, @project, @release]
   end
 
   def update
-    @view_template = @project.view_templates.find(params[:id])
-
-    respond_to do |format|
-      if @view_template.update_attributes(params[:view_template])
-        format.html { redirect_to [:admin, @project, @view_template], notice: 'View Template was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @view_template.errors, status: :unprocessable_entity }
-      end
-    end
+    @release = @project.view_templates.find(params[:id])
+    @release.update_attributes(params[:release])
+    respond_with @release, location: [:admin, @project, @release]
   end
 
   def destroy
-    @view_template = @project.view_templates.find(params[:id])
-    @view_template.destroy
+    @release = @project.view_templates.find(params[:id])
+    @release.destroy
 
-    respond_to do |format|
-      format.html { redirect_to admin_project_view_templates_url(@project) }
-      format.json { render json: '{ "status": "success" }',  status: :ok }
+    respond_with(@release, location: admin_project_view_templates_url) do |format|
+      format.json { render json: '{ "status":"success" }', status: :ok }
     end
   end
 end
