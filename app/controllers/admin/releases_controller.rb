b8b1dd8b83101480e28165989b529004b9af22a9
Winter Lee <wintafay@gmail.com>
2012-03-13 18:42:05 -0400

    updated release and view template controller with respond_with

--- app/controllers/admin/releases_controller.rb
+++ app/controllers/admin/releases_controller.rb
@@ -1,70 +1,40 @@
 class Admin::ReleasesController < AdminController
   def index
     @releases = @project.releases.order_by([sort_column, sort_direction]).page(params[:page])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @releases }
-    end
-  end
-
-  def show
-    @release = @project.releases.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @release }
-    end
+    respond_with @releases
   end
 
   def new
     @release = @project.releases.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @release }
-    end
   end
 
   def edit
     @release = @project.releases.find(params[:id])
   end
 
+  def show
+    @release = @project.releases.find(params[:id])
+    respond_with @release
+  end
+
   def create
     @release = @project.releases.new(params[:release])
-
-    respond_to do |format|
-      if @release.save
-        format.html { redirect_to admin_project_releases_url(@project), notice: 'Release was successfully created.' }
-        format.json { render json: @release, status: :created }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @release.errors, status: :unprocessable_entity }
-      end
-    end
+    @release.save
+    respond_with @release, location: [:admin, @project, @release]
   end
 
   def update
     @release = @project.releases.find(params[:id])
-
-    respond_to do |format|
-      if @release.update_attributes(params[:release])
-        format.html { redirect_to admin_project_releases_url(@project), notice: 'Release was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @release.errors, status: :unprocessable_entity }
-      end
-    end
+    @release.update_attributes(params[:release])
+    respond_with @release, location: [:admin, @project, @release]
   end
 
   def destroy
     @release = @project.releases.find(params[:id])
     @release.destroy
 
-    respond_to do |format|
-      format.html { redirect_to admin_project_releases_url(@project) }
-      format.json { render json: '{ "status": "success" }',  status: :ok }
+    respond_with(@release, location: admin_project_releases_url) do |format|
+      format.json { render json: '{ "status":"success" }', status: :ok }
     end
   end
 
@@ -77,9 +47,9 @@ class Admin::ReleasesController < AdminController
     @project.touch
     Resque.enqueue(DeployProject, @project.name)
 
-    respond_to do |format|
+    respond_with(@release) do |format|
       format.html { redirect_to(admin_project_releases_url(@project)) }
-      format.json { render json: '{ "status": "success" }',  status: :ok }
+      format.json { render json: '{ "status":"success" }',status: :ok }
     end
   end
 end
