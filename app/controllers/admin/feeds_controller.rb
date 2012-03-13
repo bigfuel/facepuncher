975d63dbd277ac5491d23f8c0e9a3693263d5e96
Winter Lee <wintafay@gmail.com>
2012-03-13 16:08:48 -0400

    updated feed controller with respond_with

--- app/controllers/admin/feeds_controller.rb
+++ app/controllers/admin/feeds_controller.rb
@@ -1,11 +1,9 @@
 class Admin::FeedsController < AdminController
+  respond_to :html, :json
+
   def index
     @feeds = @project.feeds.order_by([sort_column, sort_direction]).page(params[:page])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @feeds }
-    end
+    respond_with @feeds
   end
 
   def new
@@ -18,49 +16,27 @@ class Admin::FeedsController < AdminController
 
   def show
     @feed = @project.feeds.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @feed }
-    end
+    respond_with @feed
   end
 
   def create
     @feed = @project.feeds.new(params[:feed])
-
-    respond_to do |format|
-      if @feed.save
-        @feed.move_to_top
-        format.html { redirect_to([:admin, @project, @feed], notice: 'Feed was successfully created.') }
-        format.json { render json: @feed, status: :created, location: @feed }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
-    end
+    @feed.save
+    respond_with @feed, location: [:admin, @project, @feed]
   end
 
   def update
     @feed = @project.feeds.find(params[:id])
-
-    respond_to do |format|
-      if @feed.update_attributes(params[:feed])
-        format.html { redirect_to [:admin, @project, @feed], notice: 'Feed was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
-    end
+    @feed.update_attributes(params[:feed])
+    respond_with @feed, location: [:admin, @project, @feed]
   end
 
   def destroy
     @feed = @project.feeds.find(params[:id])
     @feed.destroy
 
-    respond_to do |format|
-      format.html { redirect_to admin_project_feeds_url(@project) }
-      format.json { render json: '{ "status": "success" }',  status: :ok }
+    respond_with(@feed, location: admin_project_feeds_url) do |format|
+      format.json { render json: '{ "status":"success" }', status: :ok }
     end
   end
 end
