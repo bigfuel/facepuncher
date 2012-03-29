52a05d33545c08fb4d7bedb5b071d9c81499fbb5
Winter Lee <wintafay@gmail.com>
2012-03-29 19:53:11 -0400

    changed Facebook graph to update per project

--- app/models/facebook_graph.rb
+++ app/models/facebook_graph.rb
@@ -12,47 +12,38 @@ module FacebookGraph
   end
 
   module Album
-    def self.get(project_id)
-      project = FacebookGraph.load_project(project_id)
-      graph = FacebookGraph.get_token(project.facebook_app_id, project.facebook_app_secret)
+    def self.update(project_name, album_name)
+      project = Project.find_by_name(project_name)
+      album = project.facebook_albums.where(name: album_name).first
 
-      albums = Hash.new
-      project.facebook_albums.each do |album|
-        begin
-          photos = graph.get_connections(album.set_id, "photos")
-        rescue Koala::Facebook::APIError
-          raise Errors::InvalidDataError, "Please double check facebook album values for #{project.name}."
-        end
-        albums[album.name] = photos
+      facebook_graph = FacebookGraph.get_token(project.facebook_app_id, project.facebook_app_secret)
+
+      begin
+        photos = facebook_graph.get_connections(album.set_id, "photos")
+      rescue Koala::Facebook::APIError
+        raise Errors::InvalidDataError, "Please check values in #{project.name}"
       end
-      albums
+
+      album.update_attributes(graph: photos)
     end
   end
 
   module Event
-    def self.get(project_id)
-      project = FacebookGraph.load_project(project_id)
-      graph = FacebookGraph.get_token(project.facebook_app_id, project.facebook_app_secret)
+    def self.update(project_name, event_name)
+      project = Project.find_by_name(project_name)
+      event = project.facebook_events.where(name: event_name).first
 
-      events = Hash.new
-      project.facebook_events.each do |event|
-        begin
-          event_list = graph.get_connections(event.name, "events")
-        rescue Koala::Facebook::APIError
-          raise Errors::InvalidDataError, "Please double check facebook event values for #{project.name}."
-        end
-        event_list.each do |e|
-          e['start_time'] = Date.parse(e['start_time'])
-        end
-        event_list = event_list.reverse.take(event.limit)
-        events[event.name] = event_list
+      facebook_graph = FacebookGraph.get_token(project.facebook_app_id, project.facebook_app_secret)
+
+      begin
+        event_list = facebook_graph.get_connections(event.name, "events")
+      rescue Koala::Facebook::APIError
+        raise Errors::InvalidDataError, "Please check values in #{project.name}"
       end
-      events
-    end
-  end
 
-  private
-  def self.load_project(project_id)
-    Project.find(project_id)
+      # event_list.each { |e| e['start_time'] = Date.parse(e['start_time']) }
+
+      event.update_attributes(graph: event_list)
+    end
   end
 end
