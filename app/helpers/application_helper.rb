33d8ede086566fcdf4b2cf62cb0e179b5346feb4
Winter Lee <wintafay@gmail.com>
2012-03-30 18:39:07 -0400

    moved fixed th width to css in admin tables, removed sort_by calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,6 @@ module ApplicationHelper
     html.html_safe
   end
 
-  def sort_by(column, title = nil)
-    title ||= column.titleize
-    css_class = (column == sort_column) && "current #{sort_direction}"
-    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, {sort: column, direction: direction}, {class: css_class}
-  end
-
   def controller_action
     "#{action_name.capitalize} #{controller_name.humanize.capitalize.singularize}"
   end
