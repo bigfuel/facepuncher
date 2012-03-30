13b3e383b9d5a2006ca3102ac5d8586e8c6e31ff
Winter Lee <wintafay@gmail.com>
2012-03-30 17:49:08 -0400

    added after_save and rss field (to store data) to feed model

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -6,14 +6,22 @@ class Feed
   field :name, type: String
   field :url, type: String
   field :limit, type: Integer, default: 10
+  field :rss, type: Hash, default: Hash.new
 
-  attr_accessible :name, :url, :limit
+  attr_accessible :name, :url, :limit, :rss
 
   belongs_to :project
 
-  paginates_per 20
+  paginates_per 5
 
   validates :name, presence: true, uniqueness: { scope: :project_id, message: "has already been used in this project." }
   validates :url, presence: true
   validates :limit, numericality: { less_than_or_equal_to: 100 }
+
+  after_save :fetch_feed
+
+  protected
+  def fetch_feed
+    RssFeeds.perform_async self.project.name, self.name
+  end
 end
