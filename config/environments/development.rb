d6de21989c9f45120c85af1591fe90975d8c8e6f
Jack Chu <kamuigt@gmail.com>
2012-04-02 13:05:48 -0400

    upgrade to Rails 3.2.3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Facepuncher::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
+  # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
@@ -22,29 +22,16 @@ Facepuncher::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Raise exception on mass assignment protection for ActiveRecord models
-  # config.active_record.mass_assignment_sanitizer = :strict
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
   config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  # Custom
-  # # Disable Rails's static asset server (Apache or nginx will already do this)
-  # config.serve_static_assets = false
-  #
-  # # Compress JavaScripts and CSS
-  # config.assets.compress = true
-  #
-  # # Don't fallback to assets pipeline if a precompiled asset is missed
-  # config.assets.compile = true
-  #
-  # # Generate digests for assets URLs
-  # config.assets.digest = true
 end
