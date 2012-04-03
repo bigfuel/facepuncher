b3b3ac99be414931344542a439e610888f872f5c
Jack Chu <kamuigt@gmail.com>
2012-04-03 16:57:48 -0400

    remove need for public_dir tmp folder, compile directly into the Rails public directory

--- app/models/deploy.rb
+++ app/models/deploy.rb
@@ -21,31 +21,28 @@ module Deploy
     release.status = nil
     release.save
 
+    assets_dir = Rails.root.join('tmp', 'project_assets').to_s
+    FileUtils.rm_r(assets_dir, secure: true) if Dir.exists?(assets_dir)
+    FileUtils.mkdir_p(assets_dir)
+
     Dir.mktmpdir(project.name) do |project_dir|
-      repo_dir = File.join(project_dir, 'repo')
-      FileUtils.mkdir_p(repo_dir)
-      public_dir = File.join(project_dir, 'public')
-      FileUtils.mkdir_p(public_dir)
-      assets_dir = Rails.root.join('tmp', 'project_assets').to_s
-      FileUtils.rm_r(assets_dir, secure: true) if Dir.exists?(assets_dir)
-      FileUtils.mkdir_p(assets_dir)
 
       begin
         # clone repo
-        clone_repo(repo_dir, project.repo, release.branch)
+        clone_repo(project_dir, project.repo, release.branch)
 
         # TODO: Alert people
         begin
-          copy_assets(repo_dir, File.join(assets_dir, project.name))
+          copy_assets(project_dir, File.join(assets_dir, project.name))
         rescue Timeout::Error => e
           release.status = e.message
           release.save
           raise
         end
 
-        compile_assets(project.name, assets_dir, public_dir)
+        compile_assets(project.name, assets_dir)
 
-        self.generate_views(project.name, repo_dir)
+        generate_views(project.name, project_dir)
 
         release.go_live
         project.touch
@@ -78,19 +75,18 @@ module Deploy
     end
   end
 
-  def self.compile_assets(project_name, assets_dir, public_dir)
+  def self.compile_assets(project_name, assets_dir)
     app = Rails.application
     public_asset_path = File.join(Rails.public_path, app.config.assets.prefix)
-    if Rails.env.development?
-      public_dir = Rails.root.join('tmp', 'public_assets', project_name)
-      FileUtils.rm_r(public_dir, secure: true) if Dir.exists?(public_dir)
-    end
 
     assets = app.assets.index.instance_variable_get(:@environment)
-    manifest_path = File.join(app.config.assets.manifest || public_dir, project_name)
+    manifest_path = File.join(app.config.assets.manifest || public_asset_path, project_name)
+
+    FileUtils.rm_r(public_asset_path, secure: true) if Dir.exists?(public_asset_path)
+    FileUtils.mkdir_p(public_asset_path)
 
     compiler = Sprockets::StaticCompiler.new(assets.index,
-                                             public_dir,
+                                             public_asset_path,
                                              app.config.assets.precompile,
                                              manifest_path: manifest_path,
                                              digest: !Rails.env.development?,
@@ -101,8 +97,6 @@ module Deploy
     manifest = File.join(manifest_path, "manifest.yml")
     raise "Couldn't find manifest.yml" unless File.exists?(manifest)
 
-    FileUtils.rm_r(File.join(public_asset_path, project_name), secure: true) if Dir.exists?(File.join(public_asset_path, project_name))
-    FileUtils.cp_r("#{public_dir}/.", public_asset_path)
     AssetSync.sync unless Rails.env.development?
     Rails.cache.write("digests:#{project_name}", YAML.load_file(manifest))
   end
